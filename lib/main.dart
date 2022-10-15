1514d6527b8a9a2582deffc7d72cdd1825a1ed0a
anas altarazi <anas.anas1998.tar@gmail.com>
2022-10-14 21:49:58 -0700

    collections example

--- lib/main.dart
+++ lib/main.dart
@@ -36,5 +36,56 @@ void main() {
   print(bmwIndex);
   int tIndex = brands.indexWhere((element) => element.startsWith('t'));
   print(tIndex);
+
+  brands.insert(1, 'ferrari');
+  print(brands[1]);
+  brands.insertAll(5, ['java', 'flutter', 'cs']);
+  print(brands[7]);
+  String jointBrands = brands.join(',');
+  print(jointBrands);
+  List<String> firstCharBrands = brands.map((e) => e[0]).toList();
+  firstCharBrands.forEach((element) => print(element));
+  print(brands.length);
+  brands.remove("tesla");
+  print(brands.length);
+  brands.removeAt(1);
+  print(brands.length);
+  brands.removeLast();
+  print(brands.length);
+  brands.removeRange(2, 4);
+  print(brands.length);
+  brands.removeWhere((element) => element == 'bmw');
+  print(brands.length);
+  brands.addAll(brands);
+  brands.addAll(brands);
+  brands.addAll(brands);
+  print(brands.length);
+  brands.replaceRange(4, 7, ['mazda', 'bmw', 'audi', 'dart']);
+  print(brands.length);
+  print(brands);
+  brands.shuffle();
+  print(brands);
+  brands.singleWhere((element) => element == 'bmw'); // will throws Exception
+  List<String> skippedBrands = brands.skip(20).toList();
+  print(skippedBrands);
+  brands.insert(20, 'ferrarimazdaaudi');
+  List<String> skippedWhileBrands =
+      brands.skipWhile((value) => value.length <= 'ferrari'.length).toList();
+  print(skippedWhileBrands);
+  brands.sort(
+    (a, b) => a.runes.last - b.runes.last,
+  );
+  print(brands);
+  List<String> tookBrands = brands.take(4).toList();
+  print(tookBrands);
+  List<String> tookWhileBrands =
+      brands.takeWhile((value) => value.length < 10).toList();
+  print(tookWhileBrands);
+  Set<String> brandsSet = brands.toSet();
+  print(brandsSet);
+  List<String> longBrands =
+      brands.where((element) => element.length > 7).toList();
+  print(longBrands);
+
   // brands.clear();
 }
