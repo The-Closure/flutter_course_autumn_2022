78c141d2147413be05376ac492ac5cceabb3de44
anas altarazi <anas.anas1998.tar@gmail.com>
2022-10-21 04:08:58 -0700

    serialization example

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,46 @@
 import 'dart:convert';
 
+class Brand {
+  String? name;
+  int? followers;
+  Address? mainAddress; //{}
+  List<Address>? branches; //{},{},{}
+}
+
+class Address {
+  String? country;
+  String? city;
+  int? postalCode;
+  Address({this.city, required this.country, required this.postalCode});
+  Map<String, dynamic> toJson() {
+    Map<String, dynamic> data = {
+      'country': country,
+      'city': city,
+      'postalCode': postalCode
+    };
+    return data;
+  }
+
+  Address.fromJson(Map<String, dynamic> data) {
+    country = data['country'];
+    city = data['city'];
+    postalCode = data['postalCode'];
+  }
+}
+
 class User {
   String? name;
   String? email;
   int? id;
+  Address? address;
+
   Map<String, dynamic> toJson() {
-    Map<String, dynamic> data = {'email': email, 'name': name, 'id': id};
+    Map<String, dynamic> data = {
+      'email': email,
+      'name': name,
+      'id': id,
+      'address': address?.toJson()
+    };
 
     return data;
   }
@@ -14,20 +49,29 @@ class User {
     email = data['email'];
     name = data['name'];
     id = data['id'];
+    address = Address.fromJson(data['address']);
   }
 
-  User({required this.email, required this.id, required this.name});
+  User(
+      {required this.email,
+      required this.id,
+      required this.name,
+      required this.address});
 }
 
 void main() {
-  User user = User(email: 'gmail.com', name: 'gmail', id: 324243);
+  User user = User(
+      email: 'gmail.com',
+      name: 'gmail',
+      id: 324243,
+      address: Address(country: 'country', postalCode: 123123));
   Map<String, dynamic> userMap = user.toJson();
   String userStr = json.encode(userMap);
   print(userStr);
   Map<String, dynamic> incomeMap = json.decode(userStr);
   User incomeUser = User.fromJson(incomeMap);
   print(
-      'name : ${incomeUser.name}, email : ${incomeUser.email}, id : ${incomeUser.id}');
+      'name : ${incomeUser.name}, email : ${incomeUser.email}, id : ${incomeUser.id}, address : ${incomeUser.address?.city}');
 
   List<User> users = [user, user, user, user];
   List<Map<String, dynamic>> usersMap = users.map((e) => e.toJson()).toList();
