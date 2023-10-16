231c5277600f45df70f1ceff2f0ae863ea9f945b
AbdalazizTll <abdalaziztll992010@gmail.com>
2023-10-16 07:32:57 -0700

    4

--- bin/dart_presudral.dart
+++ bin/dart_presudral.dart
@@ -1,77 +1,90 @@
+
 void main() {
-  // int age = 20;
-  // double average;
 
-  // // print(average);
-  // String name;
-  // bool logicOperation;
+  // TODO :
 
-  // print(age);
+  //? named contructer 
+  //? factory (deign patter) //  https://refactoring.guru/design-patterns
+  //? dio in dart http://pub.dev & http://dart.dev
+  
 
-  // var counter = 30;
+  // int x =60;
+  // int y = 80;
+// print(swapTwoNumber(50, 90));
+// print(y);
 
-  // // counter = "hello";
 
-  // dynamic book = 'The Gang of four';
+// cat1.jump(10);
 
-  // book = 20;
+// print(cat1.y);
 
-  // num numberOfBooks = 30.6;
+Animal cat = Cat(id: 1, name: "name", color: "color", y: 5, birthDate: 2);
+// cat.id = 3;
+cat.jump(20);
+print(cat.id);
 
-  // print(book.runtimeType);
+print(cat.runtimeType);
 
-  // // const int userId;
 
-  // // userId = 5;
 
-  // final String nickName;
+print(cat.name);
 
-  // // ? this is a null type in this point
-  // // print(nickName);
-  // nickName = "Alhaj";
-
-  // print(nickName);
-
-  // List<int> ages = [13,20,22,55];
-  // print(ages[2]);
-
-  Map<dynamic, dynamic> majdMarks = {1: 90, 2: 80, 3: 98, "name": "ahmad"};
-
-  print(majdMarks[1]);
-  print(majdMarks['name']);
-
-  Map<String, List<int>> nestedMap = {
-    "majd mark": [90, 80, 98, 59 + 1],
-    "abdalaziz mark": [60, 60, 60, 100]
-  };
-
-  print(nestedMap['abdalaziz mark']![3]);
-
-  print(nestedMap.keys.elementAt(0));
-
-  List<Map<String, Map<String, int>>> crazyNestedMap = [
-    {
-      "hamed": {"math 1": 57},
-      "ahmad": {"math 2": 0},
-      "abd": {"math 3": 90},
-      "majd": {"math 4": 100}
-    }
-  ];
-
-print(addTwoNumber(50,firstNumber: 20,secondNumber: 100));
-
-int x = 40;
-int y = 50;
- print(swapTwoNumber(x,y)) ;
-// print(y); 
-}
-
-addTwoNumber(int thirdNumber,{int firstNumber=0,required int secondNumber}){
-return firstNumber+secondNumber+thirdNumber;
-}
-
-(int ,int)swapTwoNumber(int firstNumber,int secondNumber){
-
-return (secondNumber,firstNumber);
+ // ! Bad foraml 
+// Cat cat3 = Animal(name: name, color: color, y: y);
 
 }
+
+class Animal extends Creature {
+  String name;
+  String color;
+  int y;
+
+
+  Animal({
+    required super.id,
+    required this.name,
+    required this.color,
+    required this.y
+  });
+
+
+  
+
+  jump(int height){
+    this.y = this.y + height; 
+  }
+}
+
+class Cat extends Animal {
+int birthDate;
+  Cat({required super.id,required super.name, required super.color, required super.y,required this.birthDate});
+
+
+
+@override
+jump(int height) {
+    y= y-height;
+  }
+
+}
+
+
+abstract class Creature {
+   int id;
+  Creature({
+    required this.id,
+  });
+
+  showId(){
+
+  }
+  
+  
+}
+
+
+// (int , int) swapTwoNumber(int firstNumber ,int secondNumber){
+ 
+//   return (secondNumber ,firstNumber);
+
+// }
